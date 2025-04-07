360643f704fa4ca2624c44cfc08a7504f3147e92
Muhammad Zaim <linkguardian878@gmail.com>
2025-04-08 00:19:02 +0800

    use sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,21 +16,23 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+            @include('layouts.side-navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+            <div class="p-4 sm:ml-64">
+                <!-- Page Heading -->
+                @isset($header)
+                    <header class="bg-white dark:bg-gray-800 shadow mb-4 rounded-lg">
+                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                            {{ $header }}
+                        </div>
+                    </header>
+                @endisset
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <!-- Page Content -->
+                <main class="bg-white dark:bg-gray-800 shadow rounded-lg p-4">
+                    {{ $slot }}
+                </main>
+            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
     </body>
