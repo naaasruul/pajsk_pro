0590e62351e1e4a486a4d0fc3b8ccc40971b016b
Muhammad Zaim <linkguardian878@gmail.com>
2025-04-10 02:01:54 +0800

    add Achievement model and migration for achievements table

---
+++ app/Models/Achievement.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Achievement extends Model
+{
+    protected $fillable = [
+        'achievement_stage',
+        'achievement_placement',
+        'score'
+    ];
+}
