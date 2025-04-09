d51f1960f1af7b14017cfc3bea301d42ebc1234a
Muhammad Zaim <linkguardian878@gmail.com>
2025-04-09 23:01:53 +0800

    cocurriculum seeder

---
+++ database/seeders/CocurriculumSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class CocurriculumSeeder extends Seeder
+{
+    public function run()
+    {
+        
+    }
+}
