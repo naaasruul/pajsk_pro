bdb4a548ad35b2613a56879e60de33169f672fe8
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-09 19:17:09 +0800

    add item on sidebar

--- resources/views/layouts/side-navigation.blade.php
+++ resources/views/layouts/side-navigation.blade.php
@@ -15,48 +15,49 @@
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
             <x-sidebar-item :route="route('dashboard')" :role="request()->routeIs('dashboard')">Dashboard</x-sidebar-item>
-            @if(auth()->user()->hasRole('admin') || auth()->user()->hasRole('teacher'))
-
-            <x-sidebar-item :route="route('students.index')" :role="request()->routeIs('students.*')">Students
-            </x-sidebar-item>
 
 
-            @endif
-
+            
+            {{-- ADMIN SIDEBAR --}}
             @if(auth()->user()->hasRole('admin'))
-            <x-sidebar-item :route="route('teachers.index')" :role="request()->routeIs('teachers.*')">Teachers
-            </x-sidebar-item>
-
+            <x-sidebar-item :route="route('teachers.index')" :role="request()->routeIs('teachers.*')">Teachers</x-sidebar-item>
+            @endif
+            
+            
+            {{-- TEACHER SIDEBAR --}}
+            @if(auth()->user()->hasRole('admin') || auth()->user()->hasRole('teacher'))
+            <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Students</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                </svg>
+          </button>
+          <ul id="dropdown-example" class="hidden py-2 space-y-2">
+            <x-sidebar-item class="pl-11" :route="route('students.index')" :role="request()->routeIs('students.*')">PAJSK</x-sidebar-item>
+            <x-sidebar-item class="pl-11" :route="route('students.index')" :role="request()->routeIs('students.*')">SEGAK</x-sidebar-item>
+      </ul>
+            <x-sidebar-item :route="route('students.index')" :role="request()->routeIs('students.*')">Activity</x-sidebar-item>
+            <x-sidebar-item :route="route('students.index')" :role="request()->routeIs('students.*')">Co-curiculum</x-sidebar-item>
+            <x-sidebar-item :route="route('students.index')" :role="request()->routeIs('students.*')">Extra Co-curiculum</x-sidebar-item>
+            <x-sidebar-item :route="route('students.index')" :role="request()->routeIs('students.*')">Presentations</x-sidebar-item>
+            
+            <x-sidebar-item :route="route('students.index')" :role="request()->routeIs('students.*')">Report</x-sidebar-item>
             @endif
 
+
+
+            
             <div class="pt-4 mt-4 space-y-2 border-t border-gray-200 dark:border-gray-700">
-                <li>
-                    <a href="{{ route('profile.edit') }}"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ request()->routeIs('profile.*') ? 'bg-gray-100 dark:bg-gray-700' : '' }}">
-                        <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                            viewBox="0 0 20 20">
-                            <path
-                                d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-                        </svg>
-                        <span class="flex-1 ms-3">Profile</span>
-                    </a>
-                </li>
-                <li>
+                <x-sidebar-item :route="route('profile.edit')" :role="request()->routeIs('profile.*')">Profile</x-sidebar-item>
+                <x-sidebar-item>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <button type="submit"
-                            class="flex w-full items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                            </svg>
-                            <span class="flex-1 ms-3">Logout</span>
+                            class="flex text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <span class="">Logout</span>
                         </button>
                     </form>
-                </li>
+                </x-sidebar-item>
             </div>
         </ul>
     </div>
