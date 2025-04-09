3bdcc0497cc1a880e003cf7bf9d21f9415a4b709
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-09 20:26:24 +0800

    for datatables display

---
+++ public/js/simple-db.js
@@ -0,0 +1,6 @@
+if (document.getElementById("default-table") && typeof simpleDatatables.DataTable !== 'undefined') {
+    const dataTable = new simpleDatatables.DataTable("#default-table", {
+        searchable: false,
+        perPageSelect: false
+    });
+}
