218f5e4272063200846994d640c2acc226782dce
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-09 20:30:41 +0800

    modify routing

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Cocuriculum;
+use App\Http\Controllers\ExtraCocuriculum;
 
 Route::get('/', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
@@ -21,6 +23,12 @@ Route::middleware('auth')->group(function () {
     // Routes accessible by admin and teachers
     Route::middleware('role:admin|teacher')->group(function () {
         Route::resource('students', StudentController::class);
+    });
+
+    // Routes accessible by teachers only
+    Route::middleware('role:teacher')->prefix('cocuriculum')->name('cocuriculum.')->group(function () {
+        Route::get('/', [Cocuriculum::class, 'index'])->name('index');
+        Route::get('/extra-cocuriculum', [ExtraCocuriculum::class, 'index'])->name('index.extra-cocuriculum');
     });
 
     // Routes accessible by admin only
