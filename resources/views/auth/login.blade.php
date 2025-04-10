012a11af7e796a1b882ade6d05c511a895c74942
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-10 12:15:21 +0800

    display any login errors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,18 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
+    
+    
+    <!-- Display Validation Errors -->
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <x-toast message="{{ $error }}" type="danger" />
+        @endforeach
+    @endif
+    
+    
     <form method="POST" action="{{ route('login') }}">
         @csrf
-
         <section class="bg-gray-50 dark:bg-gray-900">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
@@ -19,18 +27,19 @@
                         </h1>
                         <form class="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                <label for="email"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                     email</label>
                                 <input type="email" name="email" id="email" value="{{ old('email') }}"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Enter your email" required autofocus >
+                                    placeholder="Enter your email" required autofocus>
                             </div>
                             <div>
                                 <label for="password"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                 <input type="password" name="password" id="password" placeholder="••••••••"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required >
+                                    required>
                             </div>
                             <div class="flex items-center justify-between">
                                 <div class="flex items-start">
@@ -39,7 +48,8 @@
                                             class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
                                     </div>
                                     <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember
+                                            me</label>
                                     </div>
                                 </div>
                                 <a href="{{ route('password.request') }}"
@@ -47,12 +57,12 @@
                                     password?</a>
                             </div>
                             <button type="submit"
-                            
                                 class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 w-full dark:hover:text-white dark:hover:bg-gray-700 text-center">Sign
                                 in</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <a href="{{ route('register') }}"
-                                    class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                                    class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
+                                    up</a>
                             </p>
                         </form>
                     </div>
