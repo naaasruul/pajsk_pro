36223f4074e8c5f051dc3139f7073352b14573bc
Muhammad Zaim <linkguardian878@gmail.com>
2025-04-11 03:37:31 +0800

    replace achievement involvement with achievement scores, adjust achievement scores

---
+++ database/migrations/2025_04_09_171631_create_achievement_scores_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievement_scores', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('involvement_type_id')->constrained()->onDelete('cascade');
+            $table->foreignId('achievement_id')->constrained()->onDelete('cascade');
+            $table->string('score')->default('ongoing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    Schema::dropIfExists('activity_scores');
+    }
+};
