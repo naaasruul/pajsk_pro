68de06a100990404823ef2296dcdcd82466fb694
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-05-03 23:10:49 +0800

    ADD seeder for extra curricular activities

---
+++ database/seeders/ExtraCocuSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ExtraCocuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $this->call(ExtraCocuServices::class);
+        $this->call(ExtraCocuSpecialAwards::class);
+        $this->call(ExtraCocuCommunityServices::class);
+        $this->call(NilamSeeder::class);
+        $this->call(ExtraCocuTIMMS_PISA::class);
+
+
+
+        
+    }
+}
