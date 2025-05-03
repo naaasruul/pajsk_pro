b90ea1f000fb7db2692b8341d40330899053ff24
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-05-03 23:09:52 +0800

    ADD Nilam model with fillable attributes and relationships for achievement and tier

---
+++ app/Models/Nilam.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Nilam extends Model
+{
+    protected $fillable = [
+        'achievement_id',
+        'tier_id',
+        'name',
+        'point',
+    ];
+
+    public function achievement()
+    {
+        return $this->belongsTo(Achievement::class, 'achievement_id');
+    }
+
+    public function tier()
+    {
+        return $this->belongsTo(Tier::class, 'tier_id');
+    }
+}
