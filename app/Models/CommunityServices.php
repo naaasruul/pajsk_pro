9c613dade6eb3d85d6cf9fcd2a6f6646bb3224a2
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-05-03 23:09:35 +0800

    ADD CommunityServices model with fillable attributes for name and point

---
+++ app/Models/CommunityServices.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CommunityServices extends Model
+{
+    //
+    protected $fillable = [
+        'name',
+        'point',
+    ];
+}
