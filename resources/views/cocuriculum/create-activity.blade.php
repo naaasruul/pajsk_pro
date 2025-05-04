b17c3b425eb5972d832424fe0efd81ce5bd82f26
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-05-05 04:27:41 +0800

    update date input fields to use dd-mm-yyyy format and convert dates to ISO for submission

--- resources/views/cocuriculum/create-activity.blade.php
+++ resources/views/cocuriculum/create-activity.blade.php
@@ -68,7 +68,7 @@
                                 </svg>
                             </div>
                             {{-- <input id="datepicker-format" datepicker datepicker-min-date="06/04/2024" datepicker-max-date="05/05/2025" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date"> --}}
-                            <input id="date-start" datepicker   type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+                            <input id="date-start" datepicker datepicker-format="dd-mm-yyyy"  type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
                           </div>
                     </div>
 
@@ -99,8 +99,9 @@
                                   <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                                 </svg>
                             </div>
-                            <input id="date-end" datepicker   type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+                            <input id="date-end" datepicker datepicker-format="dd-mm-yyyy"   type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
                           </div>
+
                     </div>
 
                     <div class="mt-6 w-1/2">
@@ -184,44 +185,7 @@
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-6">
                     <div
                         class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-transparent ">
-                        <div>
-                            {{-- <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction"
-                                class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                                type="button">
-                                <span class="sr-only">Action button</span>
-                                Action
-                                <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m1 1 4 4 4-4" />
-                                </svg>
-                            </button>
-                            <!-- Dropdown menu -->
-                            <div id="dropdownAction"
-                                class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
-                                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                    aria-labelledby="dropdownActionButton">
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reward</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Promote</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Activate
-                                            account</a>
-                                    </li>
-                                </ul>
-                                <div class="py-1">
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete
-                                        User</a>
-                                </div>
-                            </div> --}}
-                        </div>
+                        
                         <label for="table-search" class="sr-only">Search</label>
                         <div class="relative">
                             <div
@@ -405,6 +369,16 @@
 
     <script>
         $(document).ready(function () {
+        
+     
+       
+
+            // Function to convert dd-mm-yyyy to yyyy-mm-dd
+            function convertDateToISO(date) {
+            if (!date) return null; // Handle empty date
+            const [dd, mm, yyyy] = date.split('-'); // Split the date by '-'
+            return `${yyyy}-${mm}-${dd}`; // Rearrange to yyyy-mm-dd
+            }
             $('#submit-button').on('click', function () {
                 // Collect data from the form
                 console.log(`Represent: ${$('#represent').val()}`);
@@ -422,9 +396,9 @@
                     club_id: $('#club').val(),
                     activity_place: $('#address').val(), // Collect the address from the textarea
                     category: $('#category').val(), // Collect the category dynamically
-                    datetime_start: $('#date-start').val(), // Collect the start date
+                    datetime_start: convertDateToISO($('#date-start').val()), // Collect the start date
                     time_start: $('#time-start').val(), // Collect the start time
-                    datetime_end: $('#date-end').val(), // Collect the end date
+                    datetime_end: convertDateToISO($('#date-end').val()), // Collect the end date
                     time_end: $('#time-end').val(), // Collect the end time
                     teachers: [],
                     students: [],
@@ -490,7 +464,10 @@
         
         // these 2 line to ensure past date is disabled
         $('#date-start').attr('datepicker-min-date', todayDate);
+
+        console.log($('#date-start'))
         $('#date-end').attr('datepicker-min-date', todayDate);
+
 
         });
     </script>
