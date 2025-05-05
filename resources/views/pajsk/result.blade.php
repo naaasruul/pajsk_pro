979f75c693aa78c936c46970dc6dae3ad042eb38
Muhammad Zaim <linkguardian878@gmail.com>
2025-05-06 03:41:24 +0800

    return to history instead

--- resources/views/pajsk/result.blade.php
+++ resources/views/pajsk/result.blade.php
@@ -145,7 +145,7 @@
 
                         <!-- Action Buttons -->
                         <div class="flex justify-end space-x-4">
-                            <a href="{{ route('pajsk.index') }}"
+                            <a href="{{ route('pajsk.history') }}"
                                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white">
                                 Return to List
                             </a>
