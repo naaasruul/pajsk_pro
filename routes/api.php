088b740aa74e47045f9cd08ae4cfc287243f4576
Muhammad Zaim <linkguardian878@gmail.com>
2025-05-15 01:15:56 +0800

    MISC: API setup, ignore

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\StudentCrudController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::apiResource('students', StudentCrudController::class);
+});
