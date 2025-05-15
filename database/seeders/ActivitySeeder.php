4e66cb88fa380bfe9243534474435aa64df1f4ef
Muhammad Zaim <linkguardian878@gmail.com>
2025-05-16 04:23:41 +0800

    feat: create 5 activities

--- database/seeders/ActivitySeeder.php
+++ database/seeders/ActivitySeeder.php
@@ -43,40 +43,42 @@ class ActivitySeeder extends Seeder
             $this->command->warn('No data found in the "teachers" table. Please insert data before running this seeder.');
             return;
         }
+        
+        for ($i = 0; $i < 5; $i++) {
+            // Fetch IDs from related tables
+            $placementId = Placement::inRandomOrder()->first()->id;
+            $involvementId = InvolvementType::inRandomOrder()->first()->id;
+            $achievementId = Achievement::inRandomOrder()->first()->id;
+            $clubId = Club::inRandomOrder()->first()->id;
+            $leaderId = Teacher::inRandomOrder()->first()->id;
+            $createdBy = Teacher::inRandomOrder()->first()->id;
 
-        // Fetch IDs from related tables
-        $placementId = Placement::first()->id;
-        $involvementId = InvolvementType::first()->id;
-        $achievementId = Achievement::first()->id;
-        $clubId = Club::first()->id;
-        $leaderId = Teacher::first()->id;
-        $createdBy = Teacher::first()->id;
+            // Fetch random teachers and students IDs
+            $randomTeacherIds = Teacher::inRandomOrder()->limit(3)->pluck('id')->toArray();
+            $randomStudentIds = Student::inRandomOrder()->limit(3)->pluck('id')->toArray();
 
-        // Fetch random teachers and students IDs
-        $randomTeacherIds = Teacher::inRandomOrder()->limit(3)->pluck('id')->toArray();
-        $randomStudentIds = Student::inRandomOrder()->limit(3)->pluck('id')->toArray();
+            // Add Faker instance
+            $faker = \Faker\Factory::create();
 
-        // Add Faker instance
-        $faker = \Faker\Factory::create();
-
-        // Seed example activity
-        Activity::create([
-            'represent' => 'Menghadiri',
-            'placement_id' => $placementId,
-            'involvement_id' => $involvementId,
-            'achievement_id' => $achievementId,
-            'club_id' => $clubId,
-            'category' => 'Kelab & Persatuan',
-            'activity_place' => $faker->city(),
-            'date_start' => '2025-01-01',
-            'time_start' => '10:00',
-            'date_end' => '2025-01-02',
-            'time_end' => '12:00',  
-            'leader_id' => $leaderId,
-            'created_by' => $createdBy,
-            'activity_teachers_id' => $randomTeacherIds,
-            'activity_students_id' => $randomStudentIds 
-        ]);
+            // Create a new activity
+            Activity::create([
+                'represent' => 'Menghadiri',
+                'placement_id' => $placementId,
+                'involvement_id' => $involvementId,
+                'achievement_id' => $achievementId,
+                'club_id' => $clubId,
+                'category' => 'Kelab & Persatuan',
+                'activity_place' => $faker->city(),
+                'date_start' => $faker->date(),
+                'time_start' => $faker->time(),
+                'date_end' => $faker->date(),
+                'time_end' => $faker->time(),  
+                'leader_id' => $leaderId,
+                'created_by' => $createdBy,
+                'activity_teachers_id' => json_encode($randomTeacherIds),
+                'activity_students_id' => json_encode($randomStudentIds) 
+            ]);
+        }
 
         $this->command->info('Activity seeded successfully!');
     }
