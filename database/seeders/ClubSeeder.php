322ab6fb7b952ca8308fcaee98d4cc4d0ee41f46
Muhammad Zaim <linkguardian878@gmail.com>
2025-05-20 19:36:14 +0800

    feat: comments for brevity

--- database/seeders/ClubSeeder.php
+++ database/seeders/ClubSeeder.php
@@ -13,20 +13,21 @@ class ClubSeeder extends Seeder
     public function run(): void
     {
         $clubs = [
-            ['club_name' => 'Kelab Alam Sekitar', 'category' => 'Kelab & Persatuan'],
-            ['club_name' => 'Kelab Komputer', 'category' => 'Kelab & Persatuan'],
-            ['club_name' => 'Kelab Seni Lukis', 'category' => 'Kelab & Persatuan'],
-            ['club_name' => 'Kelab Bahasa Inggeris', 'category' => 'Kelab & Persatuan'],
-            ['club_name' => 'Kelab Matematik', 'category' => 'Kelab & Persatuan'],
-            ['club_name' => 'Kelab Sukan', 'category' => 'Sukan & Permainan'],
-            ['club_name' => 'Kelab Muzik', 'category' => 'Kelab & Persatuan'],
-            ['club_name' => 'Kadet Polis', 'category' => 'Badan Beruniform'],
-            ['club_name' => 'Kadet Bomba', 'category' => 'Badan Beruniform'],
-            ['club_name' => 'Pandu Puteri', 'category' => 'Badan Beruniform'],
-            ['club_name' => 'Pengakap', 'category' => 'Badan Beruniform'],
-            ['club_name' => 'PBSM', 'category' => 'Badan Beruniform'],
-            ['club_name' => 'Puteri Islam', 'category' => 'Badan Beruniform'],
-            ['club_name' => 'TKRS', 'category' => 'Badan Beruniform'],
+            ['club_name' => 'Kelab Muzik', 'category' => 'Kelab & Persatuan'],                  // id: 1
+            ['club_name' => 'Kelab Sukan', 'category' => 'Sukan & Permainan'],                  // id: 2
+            ['club_name' => 'Kadet Polis', 'category' => 'Badan Beruniform'],                   // id: 3
+
+            ['club_name' => 'Kelab Alam Sekitar', 'category' => 'Kelab & Persatuan'],           // id: 4
+            ['club_name' => 'Kelab Komputer', 'category' => 'Kelab & Persatuan'],               // id: 5
+            ['club_name' => 'Kelab Seni Lukis', 'category' => 'Kelab & Persatuan'],             // id: 6
+            ['club_name' => 'Kelab Bahasa Inggeris', 'category' => 'Kelab & Persatuan'],        // id: 7
+            ['club_name' => 'Kelab Matematik', 'category' => 'Kelab & Persatuan'],              // id: 8
+            ['club_name' => 'Kadet Bomba', 'category' => 'Badan Beruniform'],                   // id: 9
+            ['club_name' => 'Pandu Puteri', 'category' => 'Badan Beruniform'],                  // id: 10
+            ['club_name' => 'Pengakap', 'category' => 'Badan Beruniform'],                      // id: 11
+            ['club_name' => 'PBSM', 'category' => 'Badan Beruniform'],                          // id: 12
+            ['club_name' => 'Puteri Islam', 'category' => 'Badan Beruniform'],                  // id: 13
+            ['club_name' => 'TKRS', 'category' => 'Badan Beruniform'],                          // id: 14
         ];
 
         foreach ($clubs as $club) {
