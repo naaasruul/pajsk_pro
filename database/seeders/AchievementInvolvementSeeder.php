ba86e43709db59c241850724061831c2570fa7cf
Muhammad Zaim <linkguardian878@gmail.com>
2025-05-24 15:34:18 +0800

    feat: 0 score

--- database/seeders/AchievementInvolvementSeeder.php
+++ database/seeders/AchievementInvolvementSeeder.php
@@ -17,8 +17,9 @@ class AchievementInvolvementSeeder extends Seeder
             [1, 1, 20], // International
             [1, 2, 18], // National
             [1, 3, 16], // State
-            [1, 4, 14], // District
-            [1, 5, 12], // School
+            [1, 4, 14], // District (sabah sarawak)
+            [1, 5, 12], // District 
+            [1, 6, 0], // School
             
             // Type 2 (Medium)
             [2, 1, 15],
@@ -26,6 +27,7 @@ class AchievementInvolvementSeeder extends Seeder
             [2, 3, 11],
             [2, 4, 9],
             [2, 5, 7],
+            [2, 6, 0],
             
             // Type 3 (Basic)
             [3, 1, 10],
@@ -33,6 +35,7 @@ class AchievementInvolvementSeeder extends Seeder
             [3, 3, 6],
             [3, 4, 4],
             [3, 5, 2],
+            [3, 6, 0],
         ];
 
         foreach ($scoreMatrix as [$type, $achievement, $score]) {
