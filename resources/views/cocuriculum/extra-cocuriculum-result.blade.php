3b8188c1906a5263ba5b93085b0709686708f3bc
Muhammad Zaim <linkguardian878@gmail.com>
2025-05-25 19:02:33 +0800

    fix: dont print button

--- resources/views/cocuriculum/extra-cocuriculum-result.blade.php
+++ resources/views/cocuriculum/extra-cocuriculum-result.blade.php
@@ -46,7 +46,7 @@
                                 Print
                             </button>
                             <a href="{{ route('pajsk.extra-cocuriculum.history') }}"
-                               class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white">
+                               class="no-print inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white">
                                 Return to List
                             </a>
                         </div>
