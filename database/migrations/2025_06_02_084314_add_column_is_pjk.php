3b9840f5e4894d40de4bbe2e65702ef2522b2e70
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-06-02 15:05:08 +0800

    Add migrations for subjects, subject_teacher, and classroom_subject_teacher tables; include is_pjk column in teachers table

---
+++ database/migrations/2025_06_02_084314_add_column_is_pjk.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teachers', function (Blueprint $table) {
+            //
+            $table->boolean('is_pjk')->default(false)->after('user_id')->comment('Indicates if the teacher is a Physical Education Teacher (PJK)');        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teachers', function (Blueprint $table) {
+            //
+        });
+    }
+};
