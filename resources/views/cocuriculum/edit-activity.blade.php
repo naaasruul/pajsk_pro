b72ad25494b278cf5b8babd2543badbcea2e9110
naaasruul <nasrulhaqhidayatu@gmail.com>
2025-06-12 02:36:19 +0800

    boleh edit placement bial dah aproved by admin je

--- resources/views/cocuriculum/edit-activity.blade.php
+++ resources/views/cocuriculum/edit-activity.blade.php
@@ -182,7 +182,8 @@
                     </select>
                 </div>
 
-                <div class="mt-6">
+                @if($activity->status == 'approved')
+                    <div class="mt-6">
                     <label class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300">Placement</label>
                     <select id="placement" name="placement_id"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -195,6 +196,7 @@
                         @endforeach
                     </select>
                 </div>
+                @endif
 
                 {{-- COLUMN 5 Activity Address --}}
                 <div class="mt-6">
